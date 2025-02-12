3abc760c4ff2518aad2b598e583308474424ec2f
unknown <adityaparasharadiadi12@gmail.com>
2025-02-12 19:01:13 +0530

    #3 upload recruiter functionlaity done

--- server/routes/recruiterProfile.js
+++ server/routes/recruiterProfile.js
@@ -1,12 +1,14 @@
 import express from 'express'
-import { recruiterDetails, recruiterLogin, refresh } from '../controllers/recruiterController.js';
+import { recruiterDetails, recruiterLogin, refresh, uploadRecruiter } from '../controllers/recruiterController.js';
 import { verifyToken } from '../middleware/verifyToken.js';
+import { upload } from '../utils/multer.js';
 
 const recruiterRoute = express.Router();
 
 
 
 recruiterRoute.post('/createRecruiter',recruiterDetails);
+recruiterRoute.post('/uploadRecruiter',upload.fields([{name:'csvFile',maxCount:1}]),uploadRecruiter);
 recruiterRoute.post('/loginRecruiter',recruiterLogin);
 recruiterRoute.post('/refresh',verifyToken,refresh)
 
